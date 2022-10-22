e59f51b6b3ddce288d564930cc8775fd7613d7c6
AthN3090 <amandevrishi12@gmail.com>
2022-10-22 23:30:50 +0530

    loading removed - bug

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   })
   return (
     <div className="App">
-      <Loading isVisible={loaderVisible}/>
+      {/* <Loading isVisible={loaderVisible}/> */}
       <Navigation/>
       <Home />
       <About />
